8373b4639de1943b1eb13c89a3bee6343a7d16d2
rompemuelaas <0284694@up.edu.mx>
2026-05-20 20:34:04 -0600

    Add files via upload

---
+++ CartaAsesino.java
@@ -0,0 +1,19 @@
+public class CartaAsesino extends Carta {
+    private static final String ANSI_NEGRO = "\u001B[90m"; 
+
+    // Constructor
+    public CartaAsesino(String palabra) {
+        super(palabra, 4); 
+    }
+
+    @Override
+    protected String obtenerColorANSI() {
+        return ANSI_NEGRO;
+    }
+
+    @Override
+    public String revelar() {
+        this.revelada = true;
+        return "Fin del juego! Has revelado al asesino. Tu equipo pierde inmediatamente.";
+    }
+}
